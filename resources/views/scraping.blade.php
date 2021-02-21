49843ff5dde1f0db443f54be1ae5168ae4945099
upbook26 <upbook.26@gmail.com>
2021-02-21 22:16:16 +0900

    view 一部訂正

--- resources/views/scraping.blade.php
+++ resources/views/scraping.blade.php
@@ -1,9 +1,19 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<ol class="container">
-    @foreach($titles as $title)
-    <li>{{ $title }}</li>
-    @endforeach
-</ol>
-@endsection
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Scraping</title>
+</head>
+
+<body>
+    <ol class="container">
+        @foreach($titles as $title)
+        <li>{{ $title }}</li>
+        @endforeach
+    </ol>
+</body>
+
+</html>
